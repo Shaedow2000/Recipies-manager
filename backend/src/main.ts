fe39ce44514b95edd494e4ec1fdd174c5f7beeab
Shaedow2000 <shaedow2000@proton.me>
2026-06-24 12:32:04 +0100

    Setup main server architecture

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,24 @@
-console.log("yey");
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+import type { Express } from "express";
+
+const PORT: number = parseInt(process.env.PORT || "8000", 10);
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+
+async function start(): Promise<void> {
+  try {
+    app.listen(PORT, (): void => {
+      console.log("asdf");
+    });
+  } catch (error: Error | unknown) {
+    console.log(error);
+  }
+}
+
+start();
