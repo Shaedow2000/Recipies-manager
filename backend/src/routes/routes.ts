b90fd6480da64d9915e05eb97ae2fcadbf449a0a
Shaedow2000 <shaedow2000@proton.me>
2026-06-26 12:38:10 +0100

    Add / controller

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -7,30 +7,45 @@ const router: express.Router = express.Router();
 
 const get: Get = new Get();
 
-router.get("/categories", async (_req: Request, res: Response) => {
-  const categories: string[] = await get.categories();
-
-  return res.status(200).json({
-    categories: [
-      {
-        id: 1,
-        name: categories[0],
-      },
-      {
-        id: 2,
-        name: categories[1],
-      },
-      {
-        id: 3,
-        name: categories[2],
-      },
-      {
-        id: 4,
-        name: categories[3],
-      },
-    ],
+router.get("/", async (_req: Request, res: Response): Promise<Response> => {
+  return res.status(400).json({
+    message: "This is not a valid route. Please check the routes:",
+    routes: {
+      categories: "GET /categories",
+      recipes: "GET /recipes",
+      recipe_by_id: "GET /recipes/:id",
+      new_recipe: "POST /recipes",
+    },
   });
 });
+
+router.get(
+  "/categories",
+  async (_req: Request, res: Response): Promise<Response> => {
+    const categories: string[] = await get.categories();
+
+    return res.status(200).json({
+      categories: [
+        {
+          id: 1,
+          name: categories[0],
+        },
+        {
+          id: 2,
+          name: categories[1],
+        },
+        {
+          id: 3,
+          name: categories[2],
+        },
+        {
+          id: 4,
+          name: categories[3],
+        },
+      ],
+    });
+  },
+);
 
 router.get("/recipes", () => {});
 
