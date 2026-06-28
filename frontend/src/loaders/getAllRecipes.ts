0d55605f92850f63051aa4788df12427f6616a2b
Shaedow2000 <shaedow2000@proton.me>
2026-06-28 12:26:37 +0100

    feat: add get all recipes loader for home page

---
+++ frontend/src/loaders/getAllRecipes.ts
@@ -0,0 +1,12 @@
+import type { ApiResponse } from "../types/ApiResponseType";
+import { apiFetch } from "../utils/api";
+
+async function getAllRecipes() {
+  const response: ApiResponse = await apiFetch("/recipes");
+
+  return {
+    data: response.ok ? response.data : response.error,
+  };
+}
+
+export default getAllRecipes;
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./pages/Home";
 import Root from "./layouts/Root";
+import getAllRecipes from "./loaders/getAllRecipes";
 
 const router = createBrowserRouter([
   {
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        loader: getAllRecipes,
         Component: Home,
       },
       {
