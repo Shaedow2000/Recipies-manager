4af31344d42765a72a39aa1271e6461e1d8fbb42
Shaedow2000 <shaedow2000@proton.me>
2026-06-28 12:41:30 +0100

    fix: return an ok value with loader data

--- frontend/src/loaders/getAllRecipes.ts
+++ frontend/src/loaders/getAllRecipes.ts
@@ -2,9 +2,10 @@ import type { ApiResponse } from "../types/ApiResponseType";
 import { apiFetch } from "../utils/api";
 
 async function getAllRecipes() {
-  const response: ApiResponse = await apiFetch("/recipes");
+  const response: ApiResponse = await apiFetch("recipes");
 
   return {
+    ok: response.ok,
     data: response.ok ? response.data : response.error,
   };
 }
