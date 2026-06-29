09a94d99888b44654b1adf2f9305be01931b4570
Shaedow2000 <shaedow2000@proton.me>
2026-06-29 09:02:51 +0100

    feat: show recipe details on page

--- frontend/src/pages/Details.tsx
+++ frontend/src/pages/Details.tsx
@@ -1,11 +1,38 @@
+import { useLoaderData } from "react-router";
+import type { RecipeLoader } from "../types/LoaderType";
+
 function Details() {
+  const data: RecipeLoader = useLoaderData();
+  console.log(data);
+
   return (
     <section>
       <div>
-        <h1>Details page</h1>
+        <h1>
+          {!data.ok
+            ? `An error occured: ${data.data.error.message}`
+            : `Recipe: ${data.data.recipes.name}`}
+        </h1>
       </div>
-      <div>
-        <p>details!</p>
+      <div className="recipe-info">
+        <p>Category: {data.data.recipes.category}</p>
+        <p>Preparation time: {data.data.recipes.prep_time}</p>
+        <div>
+          <p>Ingredients:</p>
+          {data.data.recipes.ingredients.map(
+            (ing: { name: string; amount: string }) => (
+              <div className="ing">
+                <p>name: {ing.name}</p>
+                <span>amount: {ing.amount}</span>
+              </div>
+            ),
+          )}
+        </div>
+        <div>
+          <p>Instructions: </p>
+          <p>{data.data.recipes.instructions}</p>
+        </div>
+        <p>Cooking time: {data.data.recipes.cook_time}</p>
       </div>
     </section>
   );
