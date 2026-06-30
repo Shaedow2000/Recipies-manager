a13d47cbad988132b657e1b7f7e8d13a61a5275a
Shaedow2000 <shaedow2000@proton.me>
2026-06-30 10:11:45 +0100

    feat: add simple loading screeen

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,44 +1,72 @@
-import { useLoaderData, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import RecipeCard from "../components/RecipeCard";
+import getAllRecipes from "../loaders/getAllRecipes";
+import { useEffect, useState } from "react";
 import type { RecipeLoader } from "../types/LoaderType";
 
 function Home() {
-  const data: RecipeLoader = useLoaderData();
+  const [data, setData]: [data: RecipeLoader, setData: any] = useState(
+    {} as any,
+  );
+  const [isLoading, setLoading]: [isLoading: boolean, setLoading: any] =
+    useState(true);
   const navigate = useNavigate();
 
+  useEffect(() => {
+    getAllRecipes()
+      .then((json) => {
+        setData(json);
+        setLoading(false);
+      })
+      .catch((error: unknown) => {
+        setData(error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <section>
-      <div className="top">
-        <h1>Home - All recipes</h1>
-        <a onClick={() => navigate("/new")}>New</a>
-      </div>
-      <div className="recipes">
-        {!data.ok ? (
-          <p>
-            An error occured: {data.data.status} - {data.data.message}
-          </p>
-        ) : data.data.recipes.length <= 0 ? (
-          <p>No recipes found</p>
-        ) : (
-          data.data.recipes.map(
-            (recipe: {
-              id: number;
-              name: string;
-              category: string;
-              ingredients: any[];
-            }) => (
-              <RecipeCard
-                id={recipe.id}
-                key={recipe.id}
-                title={recipe.name}
-                category={recipe.category}
-                numberOfIngredients={recipe.ingredients.length}
-              />
-            ),
-          )
-        )}
-      </div>
-    </section>
+    <>
+      {isLoading ? (
+        <div
+          style={{ width: "100vw", height: "100vh", backgroundColor: "green" }}
+        >
+          <p style={{ fontSize: "24px", color: "red" }}>LOADING</p>
+        </div>
+      ) : (
+        <section>
+          <div className="top">
+            <h1>Home - All recipes</h1>
+            <a onClick={() => navigate("/new")}>New</a>
+          </div>
+          <div className="recipes">
+            {!data.ok ? (
+              <p>
+                An error occured: {data.data.status} - {data.data.message}
+              </p>
+            ) : data.data.recipes.length <= 0 ? (
+              <p>No recipes found</p>
+            ) : (
+              data.data.recipes.map(
+                (recipe: {
+                  id: number;
+                  name: string;
+                  category: string;
+                  ingredients: any[];
+                }) => (
+                  <RecipeCard
+                    id={recipe.id}
+                    key={recipe.id}
+                    title={recipe.name}
+                    category={recipe.category}
+                    numberOfIngredients={recipe.ingredients.length}
+                  />
+                ),
+              )
+            )}
+          </div>
+        </section>
+      )}
+    </>
   );
 }
 
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./pages/Home";
 import Root from "./layouts/Root";
-import getAllRecipes from "./loaders/getAllRecipes";
 import Details from "./pages/Details";
 import getRecipeById from "./loaders/getRecipeById";
 import New from "./pages/New";
@@ -15,7 +14,6 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        loader: getAllRecipes,
         Component: Home,
       },
       {
